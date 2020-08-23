833ad9c11722b4948ee0526b0eec23bf3c709b0f
jerichochua <jerichochua@gmail.com>
2020-08-23 12:22:16 +0800

    Add ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,17 @@ public class Duke {
     public static void listUserTasks(Task[] userTasks, int taskCount) {
         System.out.println("\tHere are your tasks:");
         for (int i = 1; i <= taskCount; i++) {
-            System.out.println("\t" + i + ". " + userTasks[i-1].getTaskDescription());
+            System.out.format("\t%d. %s\n", i, userTasks[i-1].printTaskAndIcon());
+        }
+    }
+
+    public static void markTaskAsDone(Task[] userTasks, int taskNumber) {
+        if (userTasks[taskNumber-1].isDone()) {
+            System.out.println("\tThis task has already been marked as done.");
+        } else {
+            userTasks[taskNumber-1].markAsDone();
+            System.out.println("\tI have marked the following task as done:");
+            System.out.format("\t\t%s\n", userTasks[taskNumber-1].printTaskAndIcon());
         }
     }
 
@@ -20,6 +30,13 @@ public class Duke {
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
                 listUserTasks(userTasks, userTasksCount);
+            } else if (userInput.contains("done") && userInput.length() > 4) {
+                int taskNumber = Integer.parseInt(userInput.substring(5));
+                if (taskNumber > userTasksCount) {
+                    System.out.println("\tTask does not exist!");
+                } else {
+                    markTaskAsDone(userTasks, taskNumber);
+                }
             } else {
                 Task userTask = new Task(userInput);
                 userTasks[userTasksCount] = userTask;
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,8 +11,16 @@ public class Task {
         return taskDescription;
     }
 
+    public boolean isDone() {
+        return isDone;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718");
+    }
+
+    public String printTaskAndIcon() {
+        return String.format("[%s] %s", getStatusIcon(), getTaskDescription());
     }
 
     public void markAsDone() {
