69448a4deda8200e498ada498c27b653b5aec74c
jerichochua <jerichochua@gmail.com>
2020-08-31 16:42:58 +0800

    Extract userTasks as a class-level attribute

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int MAX_SIZE = 100;
+    private static Task[] userTasks = new Task[MAX_SIZE];
     private static int userTasksCount = 0;
 
     public static void main(String[] args) {
         String userInput;
-        Task[] userTasks = new Task[MAX_SIZE];
         Scanner in = new Scanner(System.in);
 
         System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
@@ -15,12 +15,12 @@ public class Duke {
 
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
-                listUserTasks(userTasks, userTasksCount);
+                listUserTasks(userTasksCount);
             } else if (userInput.contains("done")) {
                 int taskNumber = Integer.parseInt(userInput.substring(5));
-                markTaskAsDone(userTasks, taskNumber);
+                markTaskAsDone(taskNumber);
             } else {
-                addUserTask(userTasks, userInput);
+                addUserTask(userInput);
             }
             userInput = in.nextLine();
         }
@@ -28,7 +28,7 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
-    public static void addUserTask(Task[] userTasks, String task) {
+    public static void addUserTask(String task) {
         if (task.startsWith("todo")) {
             task = task.replace("todo", " ").trim();
             userTasks[userTasksCount] = new Todo(task);
@@ -50,14 +50,14 @@ public class Duke {
         System.out.println("\tYou now have " + userTasksCount + " task" + addS + " in your list.");
     }
 
-    public static void listUserTasks(Task[] userTasks, int taskCount) {
+    public static void listUserTasks(int taskCount) {
         System.out.println("\tHere are your tasks:");
         for (int i = 1; i <= taskCount; i++) {
             System.out.format("\t%d. %s\n", i, userTasks[i - 1]);
         }
     }
 
-    public static void markTaskAsDone(Task[] userTasks, int taskNumber) {
+    public static void markTaskAsDone(int taskNumber) {
         userTasks[taskNumber - 1].markAsDone();
         System.out.println("\tI have marked the following task as done:");
         System.out.format("\t\t%s\n", userTasks[taskNumber - 1]);
