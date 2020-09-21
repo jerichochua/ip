f942d15ac49d9c4757a172b47900b8e65275455f
jerichochua <jerichochua@gmail.com>
2020-09-21 10:49:33 +0800

    Add Javadoc comments to exception classes

--- src/main/java/duke/exception/EmptyDescriptionException.java
+++ src/main/java/duke/exception/EmptyDescriptionException.java
@@ -1,4 +1,7 @@
 package duke.exception;
 
+/**
+ * Thrown when a user attempts to add a task without including a description of the task.
+ */
 public class EmptyDescriptionException extends DukeException {
 }
